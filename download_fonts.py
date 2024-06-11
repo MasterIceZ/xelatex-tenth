a584823a24a72822487ba606d40f8b985671f5fe
Borworntat Dendurongkul <mastericez890@gmail.com>
2024-06-11 20:23:51 +0700

    fix: download FiraCode

--- download_fonts.py
+++ download_fonts.py
@@ -6,6 +6,7 @@ from GitHub repository into local fonts directory.
 import os
 import shutil
 import sys
+import requests
 from urllib.parse import urlsplit
 from urllib.request import urlopen
 
@@ -42,18 +43,14 @@ FONT_URLS = [
     'https://github.com/bBoxType/FiraGO/raw/master/Fonts/FiraGO_OTF_1001/Italic/FiraGO-BoldItalic.otf',
     'https://github.com/bBoxType/FiraGO/raw/master/Fonts/FiraGO_OTF_1001/Roman/FiraGO-ExtraBold.otf',
     'https://github.com/bBoxType/FiraGO/raw/master/Fonts/FiraGO_OTF_1001/Italic/FiraGO-ExtraBoldItalic.otf',
-    'https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Light.ttf',
-    'https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Regular.ttf',
-    'https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Medium.ttf',
-    'https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-SemiBold.ttf',
-    'https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Bold.ttf',
-    'https://github.com/tonsky/FiraCode/raw/master/distr/ttf/FiraCode-Retina.ttf',
+    'https://github.com/tonsky/FiraCode/releases/download/6.2/Fira_Code_v6.2.zip'
 ]
 
 
 def main():
     for font_url in FONT_URLS:
         download_file(font_url)
+    download_FiraCode()
 
 
 def download_file(url):
@@ -70,6 +67,42 @@ def download_file(url):
     with urlopen(url) as response, open(local_path, 'wb') as fobj:
         shutil.copyfileobj(response, fobj)
 
+def download_FiraCode():
+    owner = "tonsky"
+    repo = "FiraCode"
+    release_tag = "6.2"
+    download_path = "fonts"
+    download_filename = ""
+
+    url = f"https://api.github.com/repos/{owner}/{repo}/releases/tags/{release_tag}"
+    response = requests.get(url)
+    release_info = response.json()
+
+    for asset in release_info['assets']:
+        asset_url = asset['browser_download_url']
+        asset_name = asset['name']
+        asset_download_path = f"{download_path}/{asset_name}"
+    
+        print(f"Downloading to fonts/ directory: {url}", file=sys.stderr)
+        asset_response = requests.get(asset_url)
+        with open(asset_download_path, 'wb') as f:
+            f.write(asset_response.content)
+
+        download_filename = asset_download_path
+    
+    print(f"Unzipping {download_filename} to {download_path}/{repo}")
+    shutil.unpack_archive(download_filename, f"{download_path}/{repo}")
+
+    print(f"Deleting FiraCode zip")
+    os.remove(download_filename)
+
+    font_list = os.listdir(f"{download_path}/{repo}/ttf")
+    print(f"Moving font files")
+    for font_file in font_list:
+        shutil.move(f"{download_path}/{repo}/ttf/{font_file}", f"{download_path}/{font_file}")
+
+    print(f"Deleting FiraCode directory")
+    shutil.rmtree(f"{download_path}/{repo}")
 
 if __name__ == '__main__':
     main()
